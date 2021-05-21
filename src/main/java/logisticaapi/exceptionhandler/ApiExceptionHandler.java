119814d92fe00a4c21108dda9bee78e0b1ffe24b
lima-wendel <wchristofer@gmail.com>
2021-05-21 14:48:51 -0300

    editing class Problema to show better answer

--- src/main/java/logisticaapi/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/logisticaapi/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package logisticaapi.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,13 +18,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<Problema.Campo> campos = new ArrayList()<>();
+		
+		for (ObjectErros error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
+			String mensagem = error.getDefaultMessage();
+			
+			campos.add(new Problema.Campo(nome, mensagem));
+		}
 		
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setDataHora(LocalDateTime.now());
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente");
+		problema.setCampos(campos);
 		
-		// TODO Auto-generated method stub
+		
 		return handleExceptionInternal(ex, problema, headers, status, request);
 	}
 
