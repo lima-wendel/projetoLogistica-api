eec52f6e5e10ec33a8f09b8e181484e2e9de9e95
lima-wendel <wchristofer@gmail.com>
2021-05-23 09:21:24 -0300

    creating domain.exception and search by email in CatalogoCliente

--- src/main/java/domain/service/CatalogoClienteService.java
+++ src/main/java/domain/service/CatalogoClienteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.exception.NegocioException;
 import domain.model.Cliente;
 import domain.model.repository.ClienteRepository;
 
@@ -17,6 +18,16 @@ public class CatalogoClienteService {
 	//método para salvar e excluir um cliente
 	@Transactional //declara que o método deve ser executado dentro de uma transação, vai salvar ou tudo ou nada
 	public Cliente salvar (Cliente cliente) {
+		
+		//consultar no ClienteRepository por email
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail()) 
+				.stream()
+				.anyMatch(ClienteExistente -> !clienteExistente.equals(cliente));
+		
+		if (emailEmUso) {
+			throw new NegocioException("Já existe um cliente cadastrado com este e-mail");
+		}
+		
 		return clienteRepository.save(cliente);
 	}
 	
