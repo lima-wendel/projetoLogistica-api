951b7bc2adb2900b7f38ee562989c123fd3090a9
lima-wendel <wchristofer@gmail.com>
2021-05-24 09:05:26 -0300

    Creating Exception message for cliente with id that no exist

--- src/main/java/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/domain/service/SolicitacaoEntregaService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.model.Cliente;
 import domain.model.Entrega;
 import domain.model.StatusEntrega;
 import domain.model.repository.EntregaRepository;
@@ -18,10 +19,13 @@ public class SolicitacaoEntregaService {
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
+		Cliente cliente = clienteRepository.findById(entrega.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
 		
 		//implementação das regras de entrega. Ex: horário de entrega, entrega com motoboys etc.
 		
 		//atribuir propriedades antes de salvar:
+		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
 		entrega.setDataPedido(LocalDateTime.now());
 		
